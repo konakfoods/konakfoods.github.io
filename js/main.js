8f3f4b55818665bd5c8a249bad88d8d7b2fd14c3
konakfoods <71274970+konakfoods@users.noreply.github.com>
2020-09-14 20:19:44 +0530

    Initial commit for konakfoods.com

---
+++ js/main.js
@@ -0,0 +1,164 @@
+jQuery(document).ready(function($) {
+
+	"use strict";
+
+	$("#go-to-home").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#home").offset().top
+		}, 2000);
+	});
+	
+	
+	var slider = function() {
+		$('.nonloop-block-3').owlCarousel({
+	    center: false,
+		items: 1,
+		autoplay:true,
+		autoplayTimeout: 3000,
+	    loop: true,
+		stagePadding: 15,
+	    margin: 20,
+	    nav: true,
+		navText: ['<i class="fa fa-arrow-left"></i>', '<i class="fa fa-arrow-right"></i>'],
+	    responsive:{
+        600:{
+        	margin: 20,
+          items: 2
+        },
+        1000:{
+        	margin: 20,
+          items: 3
+        },
+        1200:{
+        	margin: 20,
+          items: 3
+        }
+	    }
+		});
+	};
+	slider();
+
+
+	var siteMenuClone = function() {
+
+		$('<div class="site-mobile-menu"></div>').prependTo('.site-wrap');
+
+		$('<div class="site-mobile-menu-header"></div>').prependTo('.site-mobile-menu');
+		$('<div class="site-mobile-menu-close "></div>').prependTo('.site-mobile-menu-header');
+		$('<div class="site-mobile-menu-logo"></div>').prependTo('.site-mobile-menu-header');
+
+		$('<div class="site-mobile-menu-body"></div>').appendTo('.site-mobile-menu');
+
+		
+
+		$('.js-logo-clone').clone().appendTo('.site-mobile-menu-logo');
+
+		$('<span class="ion-ios-close js-menu-toggle"></div>').prependTo('.site-mobile-menu-close');
+		
+
+		$('.js-clone-nav').each(function() {
+			
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
+    });
+
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+
+});
+
+$(document).ready(function() {
+
+	$(".owl-carousel").owlCarousel({
+		autoplay:true,
+		autoplayTimeout: 3000,
+		items : 4,
+		center: false,
+		loop:true,
+		responsive: {
+		600:{
+			margin: 20,
+			items: 2
+		},
+		1000:{
+			margin: 20,
+			items: 4
+		},
+		1200:{
+			margin: 20,
+			items: 4
+		}
+	}
+
+});
+});
